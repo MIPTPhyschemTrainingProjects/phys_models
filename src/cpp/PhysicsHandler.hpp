fa2b2f332aa8f9bc16c510ba757ad15a638deeda
alekseik1 <alekseik1@yandex.ru>
2018-03-04 18:06:59 +0300

    EXPERIMENT: getForce() realization, makeEvolution() (NOT WORKING) and tests for PhysicsHandler

--- src/cpp/PhysicsHandler.hpp
+++ src/cpp/PhysicsHandler.hpp
@@ -31,7 +31,17 @@ public:
      * @param p2 Second particle
      * @return Array of x, y, z components of force applied to the <u>first</u> particle
      */
-    virtual std::array<float, 3> getForce(Particle p1, Particle p2) = 0;
+    //virtual std::array<float, 3> getForce(const Particle &p1, const Particle &p2) const = 0;
+
+    // TODO: Это -- реализация только для тестирования.
+    // TODO: Этот getForce считает силу так, будто частицы соединены пружиной жесткости 0.4
+    virtual std::array<float, 3> getForce(const Particle &p1, const Particle &p2) const {
+        std::array<float, 3> res = {0, 0, 0};
+        std::transform(p1.getCoordinates().begin(), p1.getCoordinates().end(),
+                       p2.getCoordinates().begin(), res.begin(), std::minus<float>());
+        std::transform(res.begin(), res.end(), res.begin(), std::bind1st(std::multiplies<float>(), 0.4));
+        return res;
+    }
 
     PhysicsHandler(float total_time, float dt): _total_time(total_time), _dt(dt)
     {}
@@ -50,9 +60,31 @@ public:
     // TODO: и временем, заданным при создании модели
     // Чит код для паралелльного подсчета вот такой:
     // #pragma omp parallel for
+    // TODO: Проверить прагму выше. Кажется, она не работает
+    // TODO: НЕ РАБОТАЕТ! НУЖНА РАБОТА!
+    /*
     void makeEvolution() {
+        std::vector<std::array<float, 3>> total_forces;
 
+        for(auto& it1 = _all_particles.begin(); it1 != _all_particles.end(); ++it1) {
+            #pragma omp parallel for
+            std::array<float, 3> total_force = {0, 0, 0};
+            for(auto& it2 = it1++; it2 != _all_particles.end(); ++it2) {
+                std::array<float, 3> tmp_force = getForce(*it1, *it2);
+                // Complicated std::array sum. I like C++
+                std::transform(total_force.begin(), total_force.end(),
+                               tmp_force.begin(), total_force.begin(), std::plus<float>());
+            }
+            total_forces.push_back(total_force);
+        }
+        // Now, let's evolute all the particles
+        #pragma omp parallel for
+        for(auto& it = _all_particles.end(); it != _all_particles.begin(); ++it) {
+            (*it).evolute(total_forces.back(), _dt);
+            total_forces.pop_back();
+        }
     }
+     */
 };
 
 
--- tests/cpp/PhysicsHandler_TEST.cpp
+++ tests/cpp/PhysicsHandler_TEST.cpp
@@ -8,5 +8,13 @@
 TEST(PhysicsHandler, SimpleCreation) {
     // First argument -- total time, second -- dt for each iteration
     ASSERT_NO_THROW(PhysicsHandler<MaterialDot> h(20, 0.2));
-    ASSERT_ANY_THROW(PhysicsHandler h1(20, 12));
+    ASSERT_NO_THROW(PhysicsHandler<MaterialDot> h(123, 0.12412));
+}
+
+TEST(PhysicsHandler, AddParticle) {
+    MaterialDot d1(2, -12, 2, 2);
+    MaterialDot d2(-12, 1512, -0.235, 124.214214);
+    PhysicsHandler<MaterialDot> h1(100, 2);
+    ASSERT_NO_THROW(h1.addParticle(d1));
+    ASSERT_NO_THROW(h1.addParticle(d2));
 }
