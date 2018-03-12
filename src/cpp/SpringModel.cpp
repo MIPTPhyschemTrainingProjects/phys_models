e3228ac3e65b3394fc8e7f46295f9e7148813a53
alekseik1 <alekseik1@yandex.ru>
2018-03-12 13:44:52 +0300

    SpringModel: custom `k` in constructor, uses Vector and works good

---
+++ src/cpp/SpringModel.cpp
@@ -0,0 +1,32 @@
+//
+// Created by aleksei on 12.03.18.
+//
+#include <iostream>
+#include "PhysicsModel.hpp"
+#include "MaterialDot.hpp"
+
+template<typename Particle>
+class SpringModel: public PhysicsModel<Particle> {
+private:
+    double _k = 0;
+
+public:
+    SpringModel(double k, double dt): PhysicsModel<Particle>(dt), _k(k)
+            {}
+
+    Vector getForce(const Particle& p1, const Particle& p2) const {
+        return -_k*(p1.getCoordinates()-p2.getCoordinates());
+    }
+};
+
+int main() {
+    MaterialDot md1(1, 1, 1, 2);
+    double r[3] = {-1, 2, 5};
+    MaterialDot md2(r, 3);
+    SpringModel<MaterialDot> model(0.2, 0.01);
+    double total_time = 1.2;
+    model.addParticle(md1);
+    model.addParticle(md2);
+    model.makeEvolution(total_time);
+    std::cout << model.getParticle(0).getCoordinates().toArray().at(0) << std::endl;
+}
