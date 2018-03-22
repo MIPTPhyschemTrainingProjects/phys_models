f9f6dfe84540b19040ff02419388f2b23f127528
alekseik1 <alekseik1@yandex.ru>
2018-03-22 12:36:20 +0300

    Change types to double and unsigned long, separate function for calculating force, `noexcept` flags for optimizations

--- src/cpp/PhysicsModel.hpp
+++ src/cpp/PhysicsModel.hpp
@@ -20,10 +20,9 @@
 template<typename Particle>
 class PhysicsModel {
 protected:
-    std::map<long, Particle> _numerated_particles;
-    long total_particles = 0;
-    float _dt;
-    float _total_time;
+    std::map<unsigned long, Particle> _numerated_particles;
+    unsigned long total_particles = 0;
+    double _dt;
 
 public:
     /**
@@ -32,9 +31,9 @@ public:
      * @param p2 Second particle
      * @return Array of x, y, z components of force applied to the <u>first</u> particle
      */
-    virtual std::array<float, 3> getForce(const Particle &p1, const Particle &p2) const = 0;
+    virtual std::array<double, 3> getForce(const Particle &p1, const Particle &p2) const = 0;
 
-    PhysicsModel(float total_time, float dt): _dt(dt), _total_time(total_time)
+    explicit PhysicsModel(double dt): _dt(dt)
     {}
 
     /**
@@ -51,7 +50,7 @@ public:
      * @param p2 Second particle.
      * @return Usual <i>Euclidean</i> distance between them.
      */
-    float getParticlesDistance(const Particle& p1, const Particle& p2) const {
+    double getParticlesDistance(const Particle& p1, const Particle& p2) const noexcept {
         return sqrt(pow(p1.getCoordinates()[0] - p2.getCoordinates()[0], 2)
                     + pow(p1.getCoordinates()[1] - p2.getCoordinates()[1], 2)
                     + pow(p1.getCoordinates()[2] - p2.getCoordinates()[2], 2));
@@ -61,44 +60,61 @@ public:
     * Get total number of particles in model
     * @return Number of particles (type long)
     */
-    long getCount() const noexcept { return total_particles; }
+    unsigned long getCount() const noexcept { return total_particles; }
 
     /**
      * Get particle with <b>number</b>. Numbers start with 0
      * @param number The number of a particle
      * @return Reference to the particle
      */
-    Particle& getParticle(long number) { return _numerated_particles.at(number); }
+    Particle& getParticle(unsigned long number) { return _numerated_particles.at(number); }
 
     /**
-     * Make evolution for <b>dt</b> time (which was defined during Handler creation).
-     * This method counts all forces and moves all particles accordingly.
+     * Counts all forces between all particles in current time
+     * @return Map of (number, force_array), where <b>number</b> is particle number and <b>force_array</b>
+     * is array of force acting on that particle
      */
-    void makeEvolution() {
-        std::map<long, std::array<float, 3>> total_forces;
+    std::map<unsigned long, std::array<double, 3>>& _count_all_forces() const noexcept {
+        std::map<unsigned long, std::array<double, 3>> total_forces;
 
-        for(long i = 0; i < total_particles; i++) {
+        for(unsigned long i = 0; i < total_particles; i++) {
             omp_lock_t lock;
             omp_init_lock(&lock);
-            #pragma omp parallel for
-            for(long j = i+1; j < total_particles; j++) {
+            #pragma omp parallel
+            for(unsigned long j = i+1; j < total_particles; j++) {
                 omp_set_lock(&lock);
-                std::array<float, 3> tmp_force = getForce(_numerated_particles[i], _numerated_particles[j]);
+                std::array<double, 3> tmp_force = getForce(_numerated_particles[i], _numerated_particles[j]);
 
                 // Add force applied to the FIRST particle
                 total_forces[i] = tmp_force;
                 // Change force direction and add as the force applied to the SECOND particle
-                std::transform(tmp_force.begin(), tmp_force.end(), tmp_force.begin(),
-                               std::bind1st(std::multiplies<float>(), -1));
+                for(int k = 0; k < 3; k++) {
+                    tmp_force[k] = -tmp_force[k];
+                }
+                //std::transform(tmp_force.begin(), tmp_force.end(), tmp_force.begin(),
+                //               std::bind1st(std::multiplies<double>(), -1.0));
                 total_forces[j] = tmp_force;
                 omp_unset_lock(&lock);
             }
             omp_destroy_lock(&lock);
         }
+        return total_forces;
+    }
+
+    /**
+     * Make evolution for <b>dt</b> time (which was defined during Handler creation).
+     * This method counts all forces and moves all particles accordingly.
+     */
+    void makeEvolution(double time=1.0) {
         // Now, let's evolute all the particles
-        #pragma omp parallel for
-        for(long i = 0; i < total_particles; i++) {
-            _numerated_particles[i].evolute(total_forces[i], _dt);
+        std::map<unsigned long, std::array<double, 3>> total_forces =  _count_all_forces();
+        double curr_time = 0;
+        while(curr_time <= time) {
+            #pragma omp parallel for
+            for (long i = 0; i < total_particles; i++) {
+                _numerated_particles[i].evolute(total_forces[i], _dt);
+            }
+            curr_time += _dt;
         }
     }
 };
