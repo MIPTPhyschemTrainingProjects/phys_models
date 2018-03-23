e39fe8970721240ef8a738237ee8dc0ddc626a3f
alekseik1 <alekseik1@yandex.ru>
2018-03-23 19:52:30 +0300

    Statistics via `ModelState` struct

--- src/cpp/PhysicsModel.hpp
+++ src/cpp/PhysicsModel.hpp
@@ -14,6 +14,60 @@
 #include "Vector.hpp"
 
 
+struct ModelStates {
+
+    struct ParticlesInfo {
+        /// Coordinates and velocities for all particles
+        std::deque<std::array<Vector, 2>> _all_particles_info;
+
+        /**
+         * Record information about <i>particle</i>
+         * @param coords Particle coordinates
+         * @param velocity Particle velocity
+         */
+        void addParticleInfo(const Vector &coords, const Vector &velocity) { _all_particles_info.push_back({coords, velocity}); }
+
+        /**
+         * Retrieve <i>particle</i> information
+         * @param num Number of particle
+         * @return std::array<Vector, 2> with <i>coordinates</i> and <i>velocities</i>
+         */
+        std::array<Vector, 2> getParticleInfo(unsigned long num) { return _all_particles_info.at(num); };
+
+        /**
+         * Retrieve <i>particle</i> coordinates
+         * @param num Number of particle
+         * @return <b>Vector</b> of particle coordinates
+         */
+        Vector getParticleCoordinates(unsigned long num) { return _all_particles_info.at(num).at(0); }
+
+        /**
+         * Retrieve <i>particle</i> velocity
+         * @param num Number of particle
+         * @return <b>Vector</b> of particle velocity
+         */
+        Vector getParticleVelocity(unsigned long num) { return _all_particles_info.at(num).at(1); }
+    };
+
+    /// All particles' states in different iterations of evolution
+    std::deque<ParticlesInfo> _all_states;
+
+    /**
+     * Retrieve <b>ParticlesInfo</b> at given <i>iteration</t> number.
+     * @param iter Number of iteration
+     * @return <b>ParticlesInfo</b> object containing all particles' information
+     */
+    ParticlesInfo atIteration(unsigned long iter) {
+        return _all_states.at(iter);
+    }
+
+    /**
+     * Adds <b>ParticlesInfo</b> to statistics
+     * @param info <b>ParticlesInfo</b> to be added
+     */
+    void addInfo(const ParticlesInfo &info) { _all_states.push_back(info); }
+};
+
 /**
  * Class for handling physical interaction between objects <i>Particle</i> (e.g. material dots).
  * Class of <i>Particle</i> should have <b>evolute()</b> method.
@@ -26,11 +80,7 @@ protected:
     std::map<unsigned long, Particle> _numerated_particles;
     unsigned long total_particles = 0;
     double _dt;
-    std::deque<
-            std::map<unsigned long,
-                    std::array<Vector, 2>
-            >
-    > _all_states;
+    ModelStates statistics;
 
 public:
     /**
@@ -136,13 +186,13 @@ public:
         } else {
             while (curr_time <= time) {
                 std::unordered_map<unsigned long, Vector> total_forces = _count_all_forces();
-                std::map<unsigned long, std::array<Vector, 2>> tmp;
+                // Save statistics
+                ModelStates::ParticlesInfo pInfo;
                 for (unsigned long i = 0; i < total_particles; i++) {
-                    std::array<Vector, 2> _two_vec = {_numerated_particles.at(i).getCoordinates(),
-                                                      _numerated_particles.at(i).getVelocity()};
-                    tmp.insert({i, _two_vec});
+                    pInfo.addParticleInfo(_numerated_particles.at(i).getCoordinates(), _numerated_particles.at(i).getVelocity());
                 }
-                _all_states.push_back(tmp);
+                statistics.addInfo(pInfo);
+
                 #pragma omp parallel for
                 for (unsigned long i = 0; i < total_particles; i++) {
                     _numerated_particles.at(i).evolute(total_forces.at(i), _dt);
@@ -150,7 +200,6 @@ public:
                 curr_time += _dt;
             }
         }
-        std::cout << "Done\n";
     }
 };
 
